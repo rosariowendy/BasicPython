c7b067334d30b024867e14f1359243717996bc26
rosariowendy <110648988+rosariowendy@users.noreply.github.com>
2022-08-05 10:25:08 +0700

    Add files via upload

---
+++ 03 - Python Program Plot/Main.py
@@ -0,0 +1,22 @@
+import time
+start_time = time.time()
+
+print("Hello")
+print("world")
+print("halo gaes")
+
+print("hallo cuk")
+# ini adalah comment
+a = 10 # ini
+"""ada apa dengan shank si jeki
+dalam comment 
+multiline"""
+print(a)
+for i in range(1,1000):
+    a = 10
+
+print(time.time()-start_time, "second")
+# compile python ke
+# bytecode
+# cara mengcompile , buka terminal dan tuliskan
+# python -m py_compile Main.py
